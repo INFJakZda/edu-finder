0e8ca020f98d1db1dccfe86a00f94194083ab5f9
Kamil Sieniawski <kamil.sieniawski@gmail.com>
2018-11-23 13:28:30 +0100

    remove proxy module, add auth module

--- nuxt.config.js
+++ nuxt.config.js
@@ -37,7 +37,7 @@ module.exports = {
   modules: [
     // Doc: https://github.com/nuxt-community/axios-module#usage
     '@nuxtjs/axios',
-    '@nuxtjs/proxy'
+    '@nuxtjs/auth'
   ],
   /*
   ** Axios module configuration
