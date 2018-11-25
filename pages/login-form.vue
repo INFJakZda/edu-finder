8eb8be17cc07b2ba16395cf7729c97b4d8b168b7
Kamil Sieniawski <kamil.sieniawski@gmail.com>
2018-11-25 17:00:32 +0100

    add login-form

---
+++ pages/login-form.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="ui middle aligned centered grid">
+    <div class="row">
+      <Code :data="response" />
+    </div>
+    <div class="row">
+      <div class="five wide column">
+        <div
+          v-if="error"
+          class="ui bottom attached error message">
+          <i class="ban icon"/> {{ error_message }}
+        </div>
+        <h2 class="ui top attached header">Login</h2>
+        <div class="ui attached segment">
+          <form class="ui form">
+            <div class="field">
+              <label>E-mail</label>
+              <input
+                v-model="email"
+                type="text"
+                name="email"
+                placeholder="E-mail">
+            </div>
+            <div class="field">
+              <label>Password</label>
+              <input
+                v-model="password"
+                type="password"
+                name="password"
+                placeholder="Password">
+            </div>
+            <button
+              class="fluid ui button primary"
+              @click.prevent="login()">Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Code from '~/components/Code.vue'
+
+export default {
+  components: {
+    Code
+  },
+  data: function() {
+    return {
+      email: '',
+      password: '',
+      error: false,
+      error_message: '',
+      response: null
+    }
+  },
+  methods: {
+    login: function() {
+      console.log(
+        JSON.stringify({
+          email: this.email,
+          password: this.password
+        })
+      )
+      this.$axios
+        .$post('/api/account/signin', {
+          email: this.email,
+          password: this.password
+        })
+        .then(response => response.json())
+        .then(response => {
+          this.response = response
+        })
+        .catch(error => {
+          if (error.response) {
+            if (error.response.data) {
+              this.error = true
+              this.error_message =
+                'errorMessage' in error.response.data
+                  ? error.response.data.errorMessage
+                  : 'Unknown error'
+            }
+          }
+        })
+
+      console.log('Login action triggered!')
+    }
+  }
+}
+</script>
