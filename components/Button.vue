c3b82411241e3587674425838b44793296f04c8b
Kamil Sieniawski <kamil.sieniawski@gmail.com>
2018-12-14 17:06:26 +0100

    add button

---
+++ components/Button.vue
@@ -0,0 +1,48 @@
+<template>
+  <button
+    @click="onClick()"
+    :class="{ primary, secondary }">
+    {{ text }}
+  </button>
+</template>
+
+<script>
+import { isFunction } from 'lodash'
+
+export default {
+  props: {
+    text: {
+      type: String,
+      required: false,
+      default: ''
+    },
+    primary: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    secondary: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    icon: {
+      type: String,
+      required: false,
+      default: null
+    },
+    click: {
+      type: Function,
+      required: false,
+      default: null
+    }
+  },
+  methods: {
+    onClick() {
+      if (isFunction(this.click)) {
+        this.click()
+      }
+    }
+  }
+}
+</script>
