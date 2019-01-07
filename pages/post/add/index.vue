c9560f6cb90f3de58cc039669dd160b913f12d14
INFJakZda <zdanowski78@gmail.com>
2019-01-07 16:24:16 +0100

    Add template for add post

--- pages/post/add/index.vue
+++ pages/post/add/index.vue
@@ -1,11 +1,68 @@
 <template>
   <div>
-    dodda
+    <div class="ui top attached tabular menu">
+      <div class="active item">Dodaj nowy post</div>
+    </div>
+    <div class="ui bottom attached active tab segment">
+      <div class="ui equal width form">
+        <div class="fields">
+          <div class="field">
+            <label>Username</label>
+            <input 
+              type="text" 
+              placeholder="Username">
+          </div>
+          <div class="field">
+            <label>Password</label>
+            <input type="password">
+          </div>
+        </div>
+        <div class="fields">
+          <div class="field">
+            <label>First name</label>
+            <input 
+              type="text" 
+              placeholder="First Name">
+          </div>
+          <div class="field">
+            <label>Middle name</label>
+            <input 
+              type="text" 
+              placeholder="Middle Name">
+          </div>
+          <div class="field">
+            <label>Last name</label>
+            <input 
+              type="text" 
+              placeholder="Last Name">
+          </div>
+        </div>
+        <div class="fields">
+          <div class="field">
+            <label>Opis</label>
+            <textarea rows="2"/>
+          </div>
+        </div>
+        <button 
+          class="ui button"
+          @click="addPost">
+          <i class="add icon"/>
+          Dodaj nowy post
+        </button>
+      </div>
+    </div>
   </div>
+  
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    addPost() {
+      return 0
+    }
+  }
+}
 </script>
 
 <style>
