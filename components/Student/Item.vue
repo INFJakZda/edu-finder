5f3d525b665d498cecb20a545873ae262415ada1
INFJakZda <zdanowski78@gmail.com>
2019-01-25 17:37:24 +0100

    Draft post list

---
+++ components/Student/Item.vue
@@ -0,0 +1,58 @@
+<template>
+  <div 
+    class="item"
+    @click="$router.push(`/search-post/${post.id}`)">
+    <div 
+      class="ui image dev-image">
+      <div class="ui black ribbon label">
+        <span class="dev-inline"><i class="user icon"/> {{ post.author.username }} </span>
+      </div>
+      <img :src="imgSrc">
+    </div>
+        
+    <div class="content">
+
+      <div class="header">{{ post.title }}</div>
+
+      <div class="meta">
+        <span class="dev-inline"><i class="graduation cap icon"/> {{ post.skillLevel.name }} </span>
+        <span class="dev-inline"><i class="globe icon"/> {{ post.category.name }} </span>
+      </div>
+
+      <div class="description">
+        <p>{{ post.details }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    post: {
+      type: Object,
+      required: true
+    },
+    data: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    imgSrc() {
+      return `https://loli-server.azurewebsites.net/api/picture/${
+        this.post.author.avatarId
+      }`
+    }
+  },
+  methods: {
+    refreshdev() {
+      this.open = !this.open
+      this.$emit('refreshdev')
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- pages/search-post/index.vue
+++ pages/search-post/index.vue
@@ -1,12 +1,63 @@
 <template>
   <div>
-    <p>Find student</p>
+    <div class="ui breadcrumb">
+      <nuxt-link 
+        to="/" 
+        class="section">Strona startowa</nuxt-link>
+      <i class="right angle icon divider"/>
+      <div class="active section">Studenci</div>
+    </div>
+    <div class="ui link items">
+      <Item 
+        v-for="post in postData.posts"
+        :key="post.id" 
+        :post="post"
+        :data="postData"
+        @refreshdev="refreshdev"/>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+import Item from '~/components/Student/Item.vue'
+
+export default {
+  components: {
+    Item
+  },
+  asyncData(context) {
+    return context.app.$axios
+      .$get(`/api/post`)
+      .then(postData => {
+        return {
+          postData
+        }
+      })
+      .catch(e => context.error(e))
+  },
+  methods: {
+    refreshdev() {
+      this.$axios
+        .$get(`/api/post`)
+        .then(data => {
+          this.postData = data
+        })
+        .catch(e => console.log(e))
+    }
+  }
+}
 </script>
 
-<style>
+<style scoped>
+.dev-center {
+  display: flex !important;
+  justify-content: center;
+  align-items: center;
+}
+.dev-inline {
+  display: inline;
+}
+.dev-color {
+  background: #eeeeee90;
+}
 </style>
