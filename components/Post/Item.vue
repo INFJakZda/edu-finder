c30289833c5e7e7ee4d9219c0f763e46ebe7013f
INFJakZda <zdanowski78@gmail.com>
2019-01-25 15:48:38 +0100

    Add Post edit - modal works

--- components/Post/Item.vue
+++ components/Post/Item.vue
@@ -1,80 +1,21 @@
 <template lang="html">
   <div class="ui items">
-    <div 
+    <Post 
       v-for="post in posts"
       :key="post.id"
-      class="item">
-
-      <div class="ui image dev-image">
-        <div class="ui black ribbon label">
-          <span class="dev-inline"><i class="user icon"/> {{ post.author.username }} </span>
-        </div>
-        <img 
-          :src="imgSrc" 
-          class="dev-image">
-      </div>
-      
-      <div class="content">
-
-        <div class="header">{{ post.title }}</div>
-
-        <div class="meta">
-          <span class="dev-inline"><i class="graduation cap icon"/> {{ post.skillLevel.name }} </span>
-          <span class="dev-inline"><i class="globe icon"/> {{ post.category.name }} </span>
-          <span class="dev-inline"><i class="home icon"/> {{ post.city.name }} </span>
-          
-        </div>
-
-        <div class="description">
-          <p>{{ post.text }}</p>
-        </div>
-
-        <div class="extra">
-          
-          <div 
-            v-for="(tag, idx) in post.tags"
-            :key="idx"
-            class="ui label">{{ tag }}</div>
-
-          <sui-dropdown
-            class="teal ui right floated"
-            text="Opcje"
-            button
-            floating
-            direction="upward"
-          >
-            <sui-dropdown-menu>
-              <sui-dropdown-item @click.native="toggle"><sui-icon name="edit" />Edycja</sui-dropdown-item>
-              <sui-dropdown-item @click="deletePost(post.id)">
-                <sui-icon name="delete" />Usuń
-              </sui-dropdown-item>
-            </sui-dropdown-menu>
-          </sui-dropdown>
-
-          <sui-modal v-model="open">
-            <sui-modal-header>Wprowadź zmiany</sui-modal-header>
-            <sui-modal-content scrolling>
-              <Add 
-                :postdata="postdata" 
-                :postinfo="post"
-                @refreshdev="refreshdev"
-                @cancel="toggle"/>
-            </sui-modal-content>
-          </sui-modal>
-
-        </div>
-      </div>
-    </div>
+      :post="post"
+      :postdata="postdata"
+      @refreshdev="$emit('refreshdev')"/>
   </div>
 </template>
 
 <script>
-import Add from '~/components/Post/Add'
+import Post from '~/components/Post/Post.vue'
 
 export default {
   name: 'ItemGroupExample',
   components: {
-    Add
+    Post
   },
   props: {
     posts: {
@@ -85,43 +26,9 @@ export default {
       type: Object,
       required: true
     }
-  },
-  data() {
-    return {
-      open: false
-    }
-  },
-  computed: {
-    imgSrc() {
-      return this.$store.getters.imgSrc
-    }
-  },
-  methods: {
-    deletePost(id) {
-      this.$axios
-        .$delete(`/api/post/${id}`)
-        .then(() => {
-          this.$emit('refreshdev')
-        })
-        .catch(e => console.log(e))
-    },
-    toggle() {
-      this.open = !this.open
-    },
-    refreshdev() {
-      this.open = !this.open
-      this.$emit('refreshdev')
-    }
   }
 }
 </script>
 
-<style scoped>
-.dev-inline {
-  display: inline-block;
-}
-.dev-image {
-  max-width: 150px !important;
-  max-height: 150px !important;
-}
+<style>
 </style>
---
+++ components/Post/Post.vue
@@ -0,0 +1,106 @@
+<template>
+  <div class="item">
+    <div class="content">
+
+      <div class="header">{{ post.title }}</div>
+
+      <div class="meta">
+        <span class="dev-inline"><i class="graduation cap icon"/> {{ post.skillLevel.name }} </span>
+        <span class="dev-inline"><i class="globe icon"/> {{ post.category.name }} </span>
+        <span class="dev-inline"><i class="home icon"/> {{ post.city.name }} </span>
+          
+      </div>
+
+      <div class="description">
+        <p>{{ post.text }}</p>
+      </div>
+
+      <div class="extra">
+          
+        <div 
+          v-for="(tag, idx) in post.tags"
+          :key="idx"
+          class="ui label">{{ tag }}</div>
+
+        <sui-dropdown
+          class="teal ui right floated"
+          text="Opcje"
+          button
+          floating
+          direction="upward"
+        >
+          <sui-dropdown-menu>
+            <sui-dropdown-item @click.native="toggle"><sui-icon name="edit" />Edycja</sui-dropdown-item>
+            <sui-dropdown-item @click="deletePost(post.id)">
+              <sui-icon name="delete" />Usuń
+            </sui-dropdown-item>
+          </sui-dropdown-menu>
+        </sui-dropdown>
+
+        <sui-modal v-model="open">
+          <sui-modal-header>Wprowadź zmiany</sui-modal-header>
+          <sui-modal-content scrolling>
+            <Add 
+              :postdata="postdata" 
+              :postinfo="post"
+              @refreshdev="refreshdev"
+              @cancel="toggle"/>
+          </sui-modal-content>
+        </sui-modal>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Add from '~/components/Post/Add'
+
+export default {
+  components: {
+    Add
+  },
+  props: {
+    post: {
+      type: Object,
+      required: true
+    },
+    postdata: {
+      type: Object,
+      required: true
+    }
+  },
+  data() {
+    return {
+      open: false
+    }
+  },
+  methods: {
+    deletePost(id) {
+      this.$axios
+        .$delete(`/api/post/${id}`)
+        .then(() => {
+          this.$emit('refreshdev')
+        })
+        .catch(e => console.log(e))
+    },
+    toggle() {
+      this.open = !this.open
+    },
+    refreshdev() {
+      this.open = !this.open
+      this.$emit('refreshdev')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.dev-inline {
+  display: inline-block;
+}
+.dev-image {
+  max-width: 150px !important;
+  max-height: 150px !important;
+}
+</style>
