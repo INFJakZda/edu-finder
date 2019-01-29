6ad6efc842f30a11b2a1bef38b3c55e23aa48fdf
INFJakZda <zdanowski78@gmail.com>
2019-01-29 11:15:42 +0100

    Add draft user info in post

--- pages/search-post/_entryid/index.vue
+++ pages/search-post/_entryid/index.vue
@@ -9,24 +9,32 @@
         to="/search-post" 
         class="section">Studenci</nuxt-link>
       <i class="right angle icon divider"/>
-      <div class="active section">{{ entry.user.username }}</div>
+      <div class="active section">{{ entry.author.username }}</div>
     </div>
-    <div class="ui items">
-      <Details 
-        :entry="entry"
-        :data="{}"/>
+    <div class="ui card">
+      <div class="content">
+        <div class="right floated meta">14h</div>
+        <img 
+          :src="imgSrc" 
+          class="ui avatar image"> {{ entry.author.username }}
+      </div>
+      <div class="content">
+        <span class="right floated">
+          <i class="heart outline like icon"/>
+          17 likes
+        </span>
+        <i class="comment icon"/>
+        3 rekomendacje
+      </div>
+      <div class="extra content">
+        <div class="ui large transparent left icon input">
+          <i class="heart outline icon"/>
+          <input 
+            type="text" 
+            placeholder="Add Comment...">
+        </div>
+      </div>
     </div>
-    
-    <div class="ui large comments">
-      <h4 class="ui dividing header">Rekomendacje:</h4>
-      <Comment 
-        v-for="rec in entry.recommendations"
-        :key="rec.id" 
-        :data="rec"/>
-    </div>
-
-    <Education
-      :list="entry.user.educationEntries"/>
     
   </div>
 </template>
@@ -47,6 +55,17 @@ export default {
         }
       })
       .catch(e => context.error(e))
+  },
+  computed: {
+    imgSrc() {
+      if (this.entry.author.avatarId) {
+        return `https://loli-server.azurewebsites.net/api/picture/${
+          this.entry.author.avatarId
+        }`
+      } else {
+        return `http://cdn.onlinewebfonts.com/svg/img_210318.png`
+      }
+    }
   }
 }
 </script>
